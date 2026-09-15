63235b8037e36be7ad6865da41dc60621586bcd4
Oscar Gil <00186825@uca.edu.sv>
2026-09-15 10:18:04 -0600

    feat: Se creo la funcion para registrar incidencias, devuelve un resultado positivo si todo funciona

---
+++ controllers/incidenciasController.js
@@ -0,0 +1,46 @@
+const helper = require('../utils/helper.js');
+
+const incidencias = [];
+
+//! Crear una incidencia
+const crearIncidencia = (req, res) => {
+    const {empleado, area, descripcion, prioridad} = req.body;
+
+    const nuevoPaquete = {id, empleado, area, descripcion, prioridad, estado};
+    nuevoPaquete.id = helper.generarID();
+    nuevoPaquete.estado = "pendiente";
+    
+    const validaciones = [
+        {validar : helper.cadenaVacia(empleado), info : "El empleado se encuentra vacio"},
+        {validar : helper.cadenaVacia(area), info : "El area se encuentra vacia"},
+        {validar : helper.cadenaVacia(descripcion), info : "La descripcion se encuentra vacia"},
+        {validar : helper.cadenaVacia(prioridad), info : "La prioridad esta vacia"} ,
+        {validar : prioridadValidar(prioridad), info : "La prioridad con coincide con los tipos que existen"}
+    ]
+
+    const error = validaciones.find(i => i.validar);
+
+    if(error !== undefined){
+        return res.status(400).json({ error : error.info });
+    }
+
+    incidencias.push(nuevoPaquete);
+
+    res.status(200).json({mensaje : `La solicitud se guardo con el ID : ${nuevoPaquete.id}`});
+}
+
+const prioridadValidar = (prioridad) => {
+    const prioridades = {
+        alta: "alta",
+        media: "media",
+        baja: "baja",
+    };
+
+    for(const valor in prioridades){
+        if(prioridades[valor] === prioridad){
+            return true;
+        }
+    }
+
+    return false;
+};
