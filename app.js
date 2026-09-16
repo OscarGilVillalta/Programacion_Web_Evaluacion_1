496ac6892b4bdcf71db39059990a79c2d883081e
Oscar Gil <00186825@uca.edu.sv>
2026-09-15 19:25:20 -0600

    feat: Se preparo la aplicacion principal para la ejecucion

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 const express = require('express');
+const incidenciaRoutes = require('./routes/incidencias.js');
+
+
 const app = express();
 const port = 3000;
+//https://localhost:3000/
 
 app.use(express.json());
+app.use('/incidencias', incidenciaRoutes);
 
 app.listen(port, () => {
-    console.log('Hello World!');
+    console.log(`Servidor corriendo en el puerto ${port}`);
 });
